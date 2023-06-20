72acf97036fdfd0c17ff45e937ffee78ff258f69
Kathy Hoang <kathyhahoang@gmail.com>
2023-06-20 16:42:58 -0500

    Fetches supabase data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import supabase from "./supabase";
 import "./style.css";
 
 const initialFacts = [
@@ -37,7 +38,15 @@ const initialFacts = [
 
 function App() {
   const [showForm, setShowForm] = useState(false);
-  const [facts, setFacts] = useState(initialFacts);
+  const [facts, setFacts] = useState([]);
+
+  useEffect(function () {
+    async function getFacts() {
+      const { data: facts, error } = await supabase.from("facts").select("*");
+      setFacts(facts);
+    }
+    getFacts();
+  }, []);
 
   const showFormHandler = () => {
     setShowForm(() => !showForm);
--- src/supabase.js
+++ src/supabase.js
@@ -4,3 +4,5 @@ const supabaseUrl = "https://spyxpzbsoivpadnuksog.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNweXhwemJzb2l2cGFkbnVrc29nIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODcyNzE5MzAsImV4cCI6MjAwMjg0NzkzMH0.xgWdis01QgpNmxyyefTIdt0icgHP2oIyw9ugs7esPnk";
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
