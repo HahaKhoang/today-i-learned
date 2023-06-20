cc14f3f7817b953f1f943a94397da14708a66316
Kathy Hoang <kathyhahoang@gmail.com>
2023-06-20 16:29:23 -0500

    Creates Form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,228 @@
+import { useState } from "react";
 import "./style.css";
 
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
 function App() {
+  const [showForm, setShowForm] = useState(false);
+  const [facts, setFacts] = useState(initialFacts);
+
+  const showFormHandler = () => {
+    setShowForm(() => !showForm);
+  };
+
+  return (
+    <>
+      <Header
+        showForm={showForm}
+        setShowForm={setShowForm}
+        handlerFunction={showFormHandler}
+      />
+      {showForm ? (
+        <NewFactForm setFacts={setFacts} setShowForm={setShowForm} />
+      ) : null}
+
+      <main className="main">
+        <CategoryFilter />
+        <FactList facts={facts} />
+      </main>
+    </>
+  );
+}
+
+function Header({ showForm, setShowForm, handlerFunction }) {
   return (
     <header className="header">
       <div className="logo">
         <img src="logo.png" alt="Today I Learned Logo" />
         <h1>Today I Learned</h1>
       </div>
-      <button className="btn btn-large btn-open">Share a fact</button>
+      <button onClick={handlerFunction} className="btn btn-large btn-open">
+        {showForm ? "close..." : "Share a fact"}
+      </button>
     </header>
   );
 }
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+function isValidHttpUrl(string) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+function NewFactForm({ setFacts, setShowForm }) {
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("http://example.com");
+  const [category, setCategory] = useState("");
+  const textLength = text.length;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(text, source, category);
+
+    if (text && isValidHttpUrl(source) && category && text.length <= 200) {
+      const newFact = {
+        id: Math.round(Math.random() * 10000000),
+        text,
+        source,
+        category,
+        votesInteresting: 0,
+        votesMindblowing: 0,
+        votesFalse: 0,
+        createdIn: new Date().getFullYear(),
+      };
+
+      setFacts((facts) => [newFact, ...facts]);
+
+      setCategory("");
+      setSource("");
+      setText("");
+
+      setShowForm(false);
+    }
+  };
+
+  return (
+    <form className="fact-form" onSubmit={handleSubmit}>
+      {" "}
+      <input
+        type="text"
+        placeholder="Share a fact with the world..."
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+      />
+      <span>{200 - textLength}</span>
+      <input
+        type="text"
+        value={source}
+        placeholder="Trustworthy source..."
+        onChange={(event) => setSource(event.target.value)}
+      />
+      <select
+        value={category}
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value="">Choose category:</option>
+        {CATEGORIES.map((el) => (
+          <option key={el.name} value={el.name}>
+            {el.name.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
+}
+
+function CategoryFilter() {
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        {CATEGORIES.map((el) => (
+          <li key={el.name} className="category">
+            <button
+              className="btn btn-category"
+              style={{ backgroundColor: el.color }}
+            >
+              {el.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+}
+
+function FactList({ facts }) {
+  return (
+    <section>
+      <ul className="facts-list">
+        {facts.map((fact) => (
+          <Fact key={fact.id} fact={fact} />
+        ))}
+      </ul>
+      <p>There are {facts.length} facts in the database. Add your own!</p>
+    </section>
+  );
+}
+
+function Fact({ fact }) {
+  return (
+    <li className="fact">
+      <p>
+        {fact.text}
+        <a className="source" href={fact.source} target="_blank">
+          (Source)
+        </a>
+      </p>
+      <span
+        className="tag"
+        style={{
+          backgroundColor: CATEGORIES.find(
+            (category) => category.name === fact.category
+          ).color,
+        }}
+      >
+        {fact.category}
+      </span>
+      <div className="vote-buttons">
+        <button>👍 {fact.votesInteresting}</button>
+        <button>🤯 {fact.votesMindblowing}</button>
+        <button>⛔️ {fact.votesFalse}</button>
+      </div>
+    </li>
+  );
+}
 export default App;
